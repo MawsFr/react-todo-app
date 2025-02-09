b731b8b6a3a7d233500814aec0dee9b7465f03d9
Mustapha NEZZARI <mnezzari.pro@gmail.com>
2025-02-10 00:58:24 +0100

    style: add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import AddTodoForm from "@/components/add-todo-form/AddTodoForm";
+import { ThemeProvider, useTheme } from "./components/theme-provider";
 
 function App() {
-  return <AddTodoForm />;
+  const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setTheme("dark");
+  }, []);
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <AddTodoForm />
+    </ThemeProvider>
+  );
 }
 
 export default App;
