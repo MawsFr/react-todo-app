6482a90ae87b89e51fcfc8a68efe9b27ccb81711
Mustapha Nezzari <mnezzari@gmail.com>
2025-02-10 06:27:30 +0100

    chore: cleanup tailwind and shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { useEffect } from "react";
 import "./App.css";
 import AddTodoForm from "@/components/add-todo-form/AddTodoForm";
-import { ThemeProvider, useTheme } from "./components/theme-provider";
 
 function App() {
-  const { setTheme } = useTheme();
-
-  useEffect(() => {
-    setTheme("dark");
-  }, []);
-
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <AddTodoForm />
-    </ThemeProvider>
   );
 }
 
